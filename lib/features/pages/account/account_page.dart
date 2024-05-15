5253ca44814a2e9dcf355d251a9b119f4ed46979
Rahul <mrsvaz@icloud.com>
2024-05-15 17:04:32 +0530

    added

--- lib/features/pages/account/account_page.dart
+++ lib/features/pages/account/account_page.dart
@@ -1,13 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AccountPage extends StatefulWidget {
   const AccountPage({super.key});
-
   @override
   State<AccountPage> createState() => _AccountPageState();
 }
-
 class _AccountPageState extends State<AccountPage> {
   @override
   Widget build(BuildContext context) {
@@ -17,4 +14,5 @@ class _AccountPageState extends State<AccountPage> {
       ),
     );
   }
+   
 }
