2bc1c0990162dea3272d5e45087db0a8221be089
Rahul <mrsvaz@icloud.com>
2024-05-30 09:28:39 +0530

    lots of changes made

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:dil_hack_e_commerce/core/theme/palette.dart';
 import 'package:dil_hack_e_commerce/features/auth/bloc/auth_bloc.dart';
-import 'package:dil_hack_e_commerce/features/auth/presentation/login_page/login_page.dart';
 import 'package:dil_hack_e_commerce/features/pages/home/presentation/bloc/home_bloc.dart';
 import 'package:dil_hack_e_commerce/features/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -11,6 +10,22 @@ void main() {
     const MyApp(),
   );
 }
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
